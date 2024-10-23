1d9a242d249ef7eca2f2f709e2198cd93b32aa3d
AhmedGER <ahmedger713@gmail.com>
2024-10-23 18:08:38 +0300

    Update speedrunBot.js

--- bot.js
+++ bot.js
@@ -1,107 +1,251 @@
 const mineflayer = require('mineflayer');
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
-const collectBlock = require('mineflayer-collectblock');
-const autoEat = require('mineflayer-auto-eat');
-const say = require('say');
-const { Vec3 } = require('vec3');
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const movements = require('mineflayer-pathfinder').movements;
+const goals = require('mineflayer-pathfinder').goals;
+const pvp = require('mineflayer-pvp').plugin;
+const autoeat = require('mineflayer-auto-eat');
+const Vec3 = require('vec3');
+const { performance } = require('perf_hooks');
 
-// لا تحتاج لتعريف mineflayer مرة أخرى
-// const mineflayer = require('mineflayer'); // هذا السطر يجب إزالته
-
-let reconnecting = false;
-
-function createBot() {
-  const bot = mineflayer.createBot({
-    host: 'localhost', // عنوان السيرفر
-    port: 25565,       // بورت السيرفر
-    username: 'SpeedRunnerBot', // اسم البوت
-  });
-
-  bot.loadPlugin(pathfinder);
-  bot.loadPlugin(collectBlock.plugin);
-  bot.loadPlugin(autoEat.plugin);
-
-  bot.once('spawn', () => {
-    bot.autoEat.options = {
-      priority: 'foodPoints',
-      startAt: 14,
-      bannedFood: []
-    };
-
-    say.speak('I am ready to start speedrunning the game.');
-
-    const defaultMove = new Movements(bot, require('prismarine-physics')(bot.version));
-    bot.pathfinder.setMovements(defaultMove);
-
-    gatherResources(); // بدء جمع الموارد
-  });
-
-  // خوارزمية الدفاع عن نفسه
-  bot.on('entityHurt', (entity) => {
-    if (entity === bot.entity) {
-      bot.chat("I'm being attacked!");
-      defendSelf();
+// إعدادات البوت
+const botOptions = {
+    host: 'minecraft0y.aternos.me',
+    port:  30372,
+    username: 'SpeedRunBot',
+    version: '1.19.2',
+    physics: {
+        gravity: 0.08,
+        airdrag: 0.02,
+        water: 0.02,
+        lava: 0.02
     }
-  });
+};
 
-  function defendSelf() {
-    const enemy = bot.nearestEntity(e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16);
-    if (enemy) {
-      bot.chat("Engaging enemy: " + enemy.name);
-      bot.lookAt(enemy.position.offset(0, enemy.height, 0), true, () => {
-        bot.attack(enemy);
-      });
+class SpeedrunBot {
+    constructor() {
+        this.bot = mineflayer.createBot(botOptions);
+        this.setupPlugins();
+        this.setupEventHandlers();
+        this.speedrunState = {
+            hasWoodenTools: false,
+            hasIronTools: false,
+            foundStronghold: false,
+            inNether: false,
+            foundFortress: false,
+            hasBlazePowder: false,
+            hasEnderEyes: false,
+            readyForDragon: false,
+            startTime: null
+        };
     }
-  }
 
-  // تجنب هجمات التنين
-  function dodgeDragonAttack() {
-    const dragonFireball = bot.nearestEntity(e => e.name === 'ender_dragon_fireball');
-    if (dragonFireball) {
-      const fireballPos = dragonFireball.position;
-      const avoidPos = bot.entity.position.plus(new Vec3(-3, 0, -3));
-      bot.pathfinder.setGoal(new goals.GoalNear(avoidPos.x, avoidPos.y, avoidPos.z, 1));
-      bot.chat('Dodging dragon fireball!');
+    setupPlugins() {
+        this.bot.loadPlugin(pathfinder);
+        this.bot.loadPlugin(pvp);
+        this.bot.loadPlugin(autoeat);
     }
-  }
 
-  function fightEnderDragon() {
-    bot.chat("Fighting the Ender Dragon...");
-    setInterval(() => {
-      dodgeDragonAttack(); // تجنب الهجمات
-      const dragon = bot.nearestEntity(e => e.name === 'ender_dragon');
-      if (dragon) {
-        bot.chat("Attacking the dragon!");
-        bot.lookAt(dragon.position.offset(0, dragon.height / 2, 0), true, () => {
-          bot.attack(dragon);
-        });
-      }
-    }, 1000); // كرر كل ثانية
-  }
-
-  // إعادة الاتصال التلقائي
-  bot.on('end', () => {
-    if (!reconnecting) {
-      reconnecting = true;
-      bot.chat('Lost connection, reconnecting...');
-      setTimeout(() => {
-        reconnecting = false;
-        createBot(); // إعادة الاتصال
-      }, 5000); // إعادة الاتصال بعد 5 ثوانٍ
+    setupEventHandlers() {
+        this.bot.once('spawn', () => this.onSpawn());
+        this.bot.on('chat', (username, message) => this.onChat(username, message));
+        this.bot.on('entityHurt', (entity) => this.onDamage(entity));
+        this.bot.on('physicsTick', () => this.onPhysicsTick());
+        this.bot.on('end', () => this.reconnect());
     }
-  });
 
-  bot.on('goal_reached', () => {
-    bot.chat('Goal reached!');
-  });
+    // خوارزمية البداية السريعة
+    async startSpeedrun() {
+        this.speedrunState.startTime = performance.now();
+        console.log('بدء السبيد ران!');
+        
+        await this.gatherInitialResources();
+        await this.findVillageAndLoot();
+        await this.prepareForNether();
+        await this.enterAndNavigateNether();
+        await this.findStronghold();
+        await this.prepareForDragon();
+    }
 
-  bot.on('death', () => {
-    bot.chat('I died! Respawning...');
-    bot.once('spawn', () => {
-      bot.chat('Respawned! Continuing tasks...');
-      gatherResources(); // إعادة المهمة بعد الموت
-    });
-  });
+    // خوارزمية جمع الموارد الأولية
+    async gatherInitialResources() {
+        const woodTypes = ['oak_log', 'birch_log', 'spruce_log'];
+        
+        // خوارزمية البحث عن الأشجار القريبة
+        const nearestWood = this.findNearestBlocks(woodTypes, 32);
+        
+        for (const wood of nearestWood) {
+            await this.safePathTo(wood.position);
+            await this.bot.dig(wood);
+            
+            // تحقق من كفاية الخشب
+            if (this.hasEnoughWood()) break;
+        }
+
+        await this.craftBasicTools();
+        this.speedrunState.hasWoodenTools = true;
+    }
+
+    // خوارزمية البحث عن القرية ونهبها
+    async findVillageAndLoot() {
+        const villageStructures = await this.scanForStructures('village');
+        
+        if (villageStructures.length > 0) {
+            const village = villageStructures[0];
+            await this.safePathTo(village);
+            
+            // خوارزمية نهب القرية
+            await this.lootVillage();
+        }
+    }
+
+    // خوارزمية الدخول للنذر
+    async prepareForNether() {
+        if (!this.speedrunState.hasIronTools) {
+            await this.getMiningEquipment();
+        }
+        
+        // خوارزمية البحث عن الحمم
+        const lavaPool = await this.findLavaPool();
+        if (lavaPool) {
+            await this.buildNetherPortal(lavaPool);
+        }
+    }
+
+    // خوارزمية التنقل في النذر
+    async enterAndNavigateNether() {
+        this.speedrunState.inNether = true;
+        
+        // خوارزمية البحث عن قلعة النذر
+        const fortress = await this.findNetherFortress();
+        if (fortress) {
+            this.speedrunState.foundFortress = true;
+            await this.collectBlazeRods();
+        }
+    }
+
+    // خوارزمية البحث عن السترونغهولد
+    async findStronghold() {
+        if (!this.speedrunState.hasEnderEyes) {
+            await this.craftEnderEyes();
+        }
+        
+        // خوارزمية تتبع العين
+        const strongholdLocation = await this.triangulateStronghold();
+        if (strongholdLocation) {
+            this.speedrunState.foundStronghold = true;
+            await this.navigateToStronghold(strongholdLocation);
+        }
+    }
+
+    // خوارزمية الاستعداد للتنين
+    async prepareForDragon() {
+        await this.gatherEndgameEquipment();
+        this.speedrunState.readyForDragon = true;
+        await this.fightDragon();
+    }
+
+    // خوارزمية مكافحة التنين
+    async fightDragon() {
+        const dragonPhases = {
+            PERCHING: 'perching',
+            CIRCLING: 'circling',
+            CHARGING: 'charging'
+        };
+
+        while (true) {
+            const dragon = this.bot.nearestEntity(entity => entity.name === 'ender_dragon');
+            if (!dragon) break;
+
+            const phase = this.detectDragonPhase(dragon);
+            
+            switch (phase) {
+                case dragonPhases.PERCHING:
+                    await this.handlePerchingPhase(dragon);
+                    break;
+                case dragonPhases.CIRCLING:
+                    await this.handleCirclingPhase(dragon);
+                    break;
+                case dragonPhases.CHARGING:
+                    await this.avoidDragonCharge(dragon);
+                    break;
+            }
+
+            await this.destroyNearestCrystal();
+        }
+    }
+
+    // خوارزمية MLG المتقدمة
+    async handleFall() {
+        const predictedLandingSpot = this.calculateLandingPosition();
+        const fallHeight = this.bot.entity.position.y - predictedLandingSpot.y;
+        
+        if (fallHeight > 3) {
+            if (this.hasWaterBucket()) {
+                await this.performWaterMLG(predictedLandingSpot);
+            } else {
+                await this.performBlockMLG(predictedLandingSpot);
+            }
+        }
+    }
+
+    // خوارزمية تجنب المخاطر
+    async avoidDanger() {
+        const threats = this.scanForThreats();
+        
+        for (const threat of threats) {
+            const avoidanceStrategy = this.calculateAvoidanceStrategy(threat);
+            await this.executeAvoidanceManeuver(avoidanceStrategy);
+        }
+    }
+
+    // معالج الأضرار المتقدم
+    async onDamage(entity) {
+        if (entity === this.bot.entity) {
+            const health = this.bot.health;
+            const cause = this.analyzeDamageCause();
+            
+            await this.respondToDamage(health, cause);
+        }
+    }
+
+    // خوارزمية إيجاد المسار الآمن
+    async safePathTo(position) {
+        const mcData = require('minecraft-data')(this.bot.version);
+        const defaultMove = new movements(this.bot, mcData);
+        
+        defaultMove.canDig = true;
+        defaultMove.scaffoldingBlocks = ['dirt', 'cobblestone'];
+        defaultMove.scafoldingBlocks = ['dirt', 'cobblestone'];
+        
+        const goal = new goals.GoalBlock(position.x, position.y, position.z);
+        
+        try {
+            await this.bot.pathfinder.goto(goal, defaultMove);
+        } catch (error) {
+            console.log('فشل في إيجاد مسار آمن، جاري المحاولة مرة أخرى...');
+            await this.findAlternativePath(position);
+        }
+    }
+
+    // إعادة الاتصال التلقائي
+    async reconnect() {
+        console.log('انقطع الاتصال، جاري إعادة المحاولة...');
+        setTimeout(() => {
+            this.bot = mineflayer.createBot(botOptions);
+            this.setupPlugins();
+            this.setupEventHandlers();
+        }, 5000);
+    }
+
+    // خوارزمية التعامل مع الكرستالات
+    async destroyNearestCrystal() {
+        const crystal = this.bot.nearestEntity(entity => entity.name === 'end_crystal');
+        if (crystal) {
+            await this.safePathTo(crystal.position);
+            await this.bot.attack(crystal);
+        }
+    }
 }
 
-createBot();
+// إنشاء وتشغيل البوت
+const speedrunBot = new SpeedrunBot();
